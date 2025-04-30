ac38add10cab232b7dce11b549f91947ae8ef636
miloszkryzia1 <milosz.kryzia@gmail.com>
2025-04-30 13:16:53 -0700

    task 3

---
+++ Task3.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Task3 {
+
+    static void calculateBlockNumber(int numSectors, int numTracks, int numCylinders, int blockNumber) {
+        int bn = blockNumber;
+        int sectorNum = blockNumber % numSectors;
+        blockNumber /= numSectors;
+        int trackNum = blockNumber % numTracks;
+        blockNumber /= numSectors;
+        int cylinderNum = blockNumber % numCylinders;
+
+        System.out.println(String.format("The logical block number %d is located at < %d , %d , %d >", bn, cylinderNum,
+                trackNum, sectorNum));
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a logical block number: ");
+        int bn = scanner.nextInt();
+        System.out.println("Enter HD number of cylinders: ");
+        int nc = scanner.nextInt();
+        System.out.println("Enter HD number of tracks: ");
+        int nt = scanner.nextInt();
+        System.out.println("Enter HD number of sectors: ");
+        int ns = scanner.nextInt();
+        scanner.close();
+
+        calculateBlockNumber(ns, nt, nc, bn);
+    }
+
+}
